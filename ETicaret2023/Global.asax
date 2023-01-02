05f849dfe151975914957edc36dcc3aa42c5f41d
ozdemirgulsah <gulsahozdmr12@gmail.com>
2023-01-02 14:33:05 +0300

    Add project files.

---
+++ ETicaret2023/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ETicaret2023.MvcApplication" Language="C#" %>
---
+++ ETicaret2023/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETicaret2023.Startup))]
+namespace ETicaret2023
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
